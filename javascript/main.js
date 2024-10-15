5e4b0cd1e95fa1e83a0d0772cfe72abe067644b3
Simon <simontunje@hotmail.com>
2024-10-15 13:33:53 +0200

    aaaand done!

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+console.log('hello world!');
+
+let a = 'Simon';
+let b = 32;
+let c = 'green';
+let d = 10;
+let e = 25;
+let f = 3;
+
+console.log(`to cross the brige of death you must first awnser these questions three!`);
+console.log(`very well!`);
+console.log(`What is your name?`);
+console.log(`king ${a} of the birttons`);
+console.log(`What is your age?`);
+console.log(`i am ${b} years old`);
+console.log(`what is your favorit colour?`);
+console.log(`Blue! no! ${c}! AHHHHHHHH`);
+console.log(`<flies into ravine>`);
+
+
+
+console.log(`But do you know maaaths?`);
+console.log(`Yes?`);
+console.log(`really prove it!?`);
+console.log(`Well! ${d} times ${f} plus ${e} is ${d*f+e}`);
+console.log(`Dosent count!`);
+console.log(`No! Dont throw me into the ravine again! AAAHHHHHHHH!`);
+console.log(`<flies into ravine again>`);
+
+
+
+
+
+
